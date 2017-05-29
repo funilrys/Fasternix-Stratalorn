654b07491f5e41bfc919c5f6889f4a3371cc4e19
funilrys <contact@funilrys.com>
2017-05-29 23:08:56 +0200

    Add Process()

--- fasternix_stratalorn/process.py
+++ fasternix_stratalorn/process.py
@@ -1,1 +1,10 @@
 #!/bin/env python
+
+class Process(object):
+    """A class to manipulate shell commands
+
+    :param command: A string, the command to execute
+    """
+
+    def __init__(self, command):
+        self.command = command
