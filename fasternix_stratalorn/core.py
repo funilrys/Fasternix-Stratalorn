c125f9faabb4db81d7a7342ef0e0cd699d76f50d
funilrys <contact@funilrys.com>
2017-05-29 23:21:09 +0200

    Add QUERY_DESTINATION + Core().execute_save_cmd()

--- fasternix_stratalorn/core.py
+++ fasternix_stratalorn/core.py
@@ -1,4 +1,6 @@
 #!/bin/env python
+from helpers import write_file
+from process import Process
 
 class Core(object):
     """Brain of the program. Get the list of translators from a Transifex project"""
@@ -11,6 +13,7 @@ class Core(object):
         self.BASE_URL = 'http://www.transifex.com/api/2/project/' + self.project_slug
         self.ID = self.username + ':' + self.password
         self.OUTPUT_DESTINATION = './translators.json'
+        self.QUERY_OUTPUT_DESTINATION = './query_results/'
 
         self.URL_DETAILS = self.BASE_URL + '/?details'
         self.URL_LANGUAGE = self.BASE_URL + '/language/'
@@ -19,3 +22,16 @@ class Core(object):
 
         self.languages = []
         self.translators = []
+
+    def execute_save_cmd(self,command, destination):
+        """Execute and save the result of a command into a defined file.
+
+        :param command: A string, the command to execute in shell format
+        :param destination: A string, the location where we're going to save the result(s)
+        """
+
+        result = Process(command).execute()
+        destination = self.OUTPUT_DESTINATION + destination
+
+        write_file(result,destination)
+        return 
