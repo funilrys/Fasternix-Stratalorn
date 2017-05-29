5be52f784fb666bf61735c7fa5ea6e6b61ab1638
funilrys <contact@funilrys.com>
2017-05-29 23:38:31 +0200

    Add get_translators()

--- fasternix_stratalorn/__init__.py
+++ fasternix_stratalorn/__init__.py
@@ -1,1 +1,7 @@
 #!/bin/env python
+from core import Core
+
+def get_translators(username,password,project_slug):
+    """Send data and perform Core()"""
+
+    return Core(username,password,project_slug).get()
