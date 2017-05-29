fc179f3a7e57585ca2de250a38eaafe3f1b30fef
funilrys <contact@funilrys.com>
2017-05-29 23:39:31 +0200

    Add a way to handle Transifex data + call Core().get()

--- fasternix_stratalorn/__init__.py
+++ fasternix_stratalorn/__init__.py
@@ -1,7 +1,15 @@
 #!/bin/env python
 from core import Core
+from getpass import getpass
 
 def get_translators(username,password,project_slug):
     """Send data and perform Core()"""
 
     return Core(username,password,project_slug).get()
+
+
+username = input('Transifex username: ')
+password = getpass('Transifex password: ')
+project_slug = input('Transifex project_slug: ')
+
+get_translators(username,password,project_slug)
