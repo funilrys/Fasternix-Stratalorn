f87451b6556fa58a8a7f5ab0b9862faa64f58328
funilrys <contact@funilrys.com>
2017-05-29 23:06:08 +0200

    Add URL_DETAILS

--- fasternix_stratalorn/core.py
+++ fasternix_stratalorn/core.py
@@ -11,4 +11,5 @@ class Core(object):
         self.BASE_URL = 'http://www.transifex.com/api/2/project/' + self.project_slug
         self.ID = self.username + ':' + self.password
         self.OUTPUT_DESTINATION = './translators.json'
-        
+
+        self.URL_DETAILS = self.BASE_URL + '/?details'
