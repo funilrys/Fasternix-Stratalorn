2465eff0ecddb3a66f020f1e303450354c599aed
funilrys <contact@funilrys.com>
2017-05-30 00:24:06 +0200

    Add licence header

--- fasternix_stratalorn/core.py
+++ fasternix_stratalorn/core.py
@@ -1,4 +1,24 @@
 #!/bin/env python
+
+#    Fasternix Stratalorn -  Python module/library for saving the list of translators of a given Transifex project into a JSON file.
+
+#    Copyright (C) 2017  Funilrys - Funilrys - Nissar Chababy <contact at funilrys dot com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#    https://github.com/funilrys/Fasternix-Stratalorn
+
 from helpers import write_file, read_file, convert_JSON_to_dict, save_dict_to_JSON
 from os import path, makedirs
 from process import Process
