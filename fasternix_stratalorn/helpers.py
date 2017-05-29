1bce5d191549d9f42ed97173539953f5ecd4fa00
funilrys <contact@funilrys.com>
2017-05-29 23:21:29 +0200

    Add write_file()

--- fasternix_stratalorn/helpers.py
+++ fasternix_stratalorn/helpers.py
@@ -1,1 +1,11 @@
 #!/bin/env python
+
+def write_file(content,destination):
+    """Write a content in a given file
+
+    :param content: A string, the content we have to write into the file
+    :param destination: A string, A path to a file where we're going to save the content
+    """
+
+    with open(destination,'w') as file:
+        file.write(content)
