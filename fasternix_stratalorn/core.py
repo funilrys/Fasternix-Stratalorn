549d82aa3c109a5f49129d5be3fb0d8121d2b3dd
funilrys <contact@funilrys.com>
2017-10-29 08:55:38 +0100

    Review of structure + Update docstrings

--- fasternix_stratalorn/core.py
+++ fasternix_stratalorn/core.py
@@ -22,7 +22,16 @@ from .process import Process
 
 
 class Core(object):
-    """Brain of the program. Get the list of translators from a Transifex project"""
+    """
+    Get the list of translators from a Transifex project.
+
+    :param username: A string, A Transifex username. (Must be a maintainer of the project)
+    :param password: A string, the password.
+    :param project_slug: A string, A Transifex project_slug.
+    :param return_dict: A bool, True = return a dict.
+    :param return_list: A bool, True = return a list.
+    :param save_in_file: A bool, True = save in file.
+    """
 
     def __init__(self, username, password, project_slug, **args):
         self.username = username
@@ -53,7 +62,9 @@ class Core(object):
         self.translators = []
 
     def get_translated_languages(self):
-        """Get the list of translated languages in the project."""
+        """
+        Get the list of translated languages in the project.
+        """
 
         cmd = self.COMMAND_BASE + self.URL_DETAILS
         destination = self.QUERY_OUTPUT_DESTINATION + 'details.json'
@@ -68,10 +79,12 @@ class Core(object):
         return content
 
     def format_result(self):
-        """Return a formated list or a formated dict"""
+        """
+        Return a formated list or a formated dict.
+        """
 
         translators_formated_list = format_list(self.translators)
-        formated_dict = {'translators':  translators_formated_list}
+        formated_dict = {'translators': translators_formated_list}
 
         if self.return_list and self.return_dict or self.return_dict:
             self.result = formated_dict
@@ -81,7 +94,9 @@ class Core(object):
             self.result = formated_dict
 
     def save_dict(self):
-        """Save result into a file"""
+        """
+        Save result into a file.
+        """
 
         if self.save_in_file and isinstance(self.result, dict):
             save_dict_to_JSON(self.result, self.OUTPUT_DESTINATION)
@@ -89,7 +104,9 @@ class Core(object):
         return False
 
     def get_list_translators(self):
-        """Get the list of translators of the project."""
+        """
+        Get the list of translators of the project.
+        """
 
         if self.languages != []:
             if self.return_list == False and self.return_dict == False:
@@ -101,8 +118,8 @@ class Core(object):
             for language in self.languages:
                 cmd = self.COMMAND_BASE + self.URL_LANGUAGE + language
 
-                transifex_lang_output = convert_JSON_to_dict(execute_return_cmd(
-                    cmd))
+                transifex_lang_output = convert_JSON_to_dict(
+                    execute_return_cmd(cmd))
 
                 self.translators.extend(transifex_lang_output['translators'])
 
@@ -117,8 +134,9 @@ class Core(object):
         return False
 
     def get(self):
-        """Get language, then get the list of translators.
-        In between, we return an error in case we can't get information from transifex
+        """
+        Get language, then get the list of translators.
+        In between, we return an error in case we can't get information from transifex.
         """
 
         if not path.exists(self.QUERY_OUTPUT_DESTINATION):
@@ -130,8 +148,10 @@ class Core(object):
                 rmtree(self.QUERY_OUTPUT_DESTINATION)
                 clear_screen()
                 if self.save_dict() and self.return_list == False and self.return_dict == False:
-                    print('You can find your list of translators into \033[1m\033[93m%s \033[96m=)\033[0m' %
-                          path.abspath(self.OUTPUT_DESTINATION))
+                    print(
+                        'You can find your list of translators into \033[1m\033[93m%s \033[96m=)\033[0m' %
+                        path.abspath(
+                            self.OUTPUT_DESTINATION))
                     exit()
                 return self.result
 
